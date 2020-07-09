76f47ed2165bd32bd54d16236c917364f7279c2f
Janne Paalijarvi <jpaalija@gmail.com>
2020-07-09 22:53:43 +0300

    Threader read works also

--- src/diskcont.c
+++ src/diskcont.c
@@ -198,59 +198,12 @@ static void DC_PrintProgress(tDcState* pxState, uint8_t u8ForcePrint)
     pxState->xLastTime = pxState->xNowTime;
     pxState->u64LastDataLeftBytes = pxState->u64NowDataLeftBytes;
   }
-  /*
-  // Staticed here just for efficiency
-  static uint32_t u32TimeElapsed;
-  static uint32_t u32Secs;
-  static uint32_t u32Mins;
-  static uint32_t u32Hours;
-  static uint64_t u64PassedBytes;
-  static float fProgress;
-  static float fTimeElapsedFine;
-  static float fNowSpeedMbPerSeconds;
-  static float fAverageSpeedMbPerSeconds;
-  
-  u32TimeElapsed = pxState->xNowTime.tv_sec - pxState->xStartTime.tv_sec;
-  u32Secs = u32TimeElapsed % 60;
-  u32TimeElapsed -= u32Secs;
-  u32Mins = (u32TimeElapsed % 3600) / 60;
-  u32TimeElapsed -= u32Mins * 60;
-  u32Hours = u32TimeElapsed / 3600;
-  u64PassedBytes = pxState->u64DevSizeBytes - pxState->u64DataLeftBytes;
-  fProgress = 100.0 * (1.0 * u64PassedBytes) / (1.0 * pxState->u64DevSizeBytes);
-
-  // For calculations we need fine-grained stuff.
-  fNowSpeedMbPerSeconds = 0.0;
-  fAverageSpeedMbPerSeconds = 0.0;
-  
-  if (timercmp(&(pxState->xLastTime), &(pxState->xNowTime), <) && (pxState->u64LastDataLeftBytes))
-  {
-    // First calculate current speed
-    fTimeElapsedFine = (1.0 * (pxState->xNowTime.tv_sec - pxState->xLastTime.tv_sec)) +
-      (0.000001 * (pxState->xNowTime.tv_usec - pxState->xLastTime.tv_usec));
-    fNowSpeedMbPerSeconds = (1.0 * (pxState->u64LastDataLeftBytes - pxState->u64DataLeftBytes)) /
-      ((1.0 * ADT_BYTES_IN_MEBIBYTE) * fTimeElapsedFine);
-    // And now we calculate average speed
-    fTimeElapsedFine = (1.0 * (pxState->xNowTime.tv_sec - pxState->xStartTime.tv_sec)) +
-      (0.000001 * (pxState->xNowTime.tv_usec - pxState->xStartTime.tv_usec));
-    fAverageSpeedMbPerSeconds = (1.0 * (pxState->u64DevSizeBytes - pxState->u64DataLeftBytes)) /
-      ((1.0 * ADT_BYTES_IN_MEBIBYTE) * fTimeElapsedFine);
-  }
-
-  printf("\x1b[A" "\x1b[A" "\r%" PRIu64 "/%" PRIu64 " bytes, %02.2f%% done. \n"
-	 "%uh %02um %02us elapsed. \n"
-	 "Speed now: %.2f MiB/s  Average: %.2f MiB/s       ",
-	 u64PassedBytes, pxState->u64DevSizeBytes, fProgress,
-	 u32Hours, u32Mins, u32Secs, fNowSpeedMbPerSeconds, fAverageSpeedMbPerSeconds);
-  fflush(stdout);
-  */
 }
 
 
-
+/*
 static uint8_t bDC_ReadTest(tDcState* pxState)
 {
-  /*
   void* pCompBufMem = NULL;
   void* pReadBufMem = NULL;
   int iFd = -1;
@@ -349,10 +302,10 @@ static uint8_t bDC_ReadTest(tDcState* pxState)
   free(pCompBufMem);
   free(pReadBufMem);
   close(iFd);
-  */
+
   return 1;
 }
-
+*/
 
 
 static uint8_t bDC_BufferAllocator(void* pParams)
@@ -394,6 +347,15 @@ static uint8_t bDC_WriteTest(tDcState* pxState)
   uint64_t u64WriteBufferNum = 0;
   int iFd = -1;
 
+  if ((sem_init(&(pxState->xSemThread), 0, 0) != 0) ||
+      (sem_init(&(pxState->xSemBuffer0), 0, 0) != 0) ||
+      (sem_init(&(pxState->xSemBuffer1), 0, 0) != 0))
+  {
+    printf("Failed to initialize semaphores\n");
+
+    return 1;
+  }
+
   pthread_create(&(pxState->xAllocatorThread), NULL,
 		 (void*)bDC_BufferAllocator, pxState);
 
@@ -407,6 +369,9 @@ static uint8_t bDC_WriteTest(tDcState* pxState)
     printf("Error: Malloc failed\n");
     free(pxState->apMemBufs[0]);
     free(pxState->apMemBufs[1]);
+    sem_destroy(&(pxState->xSemThread));
+    sem_destroy(&(pxState->xSemBuffer0));
+    sem_destroy(&(pxState->xSemBuffer1));
     
     return 0;
   }
@@ -478,10 +443,14 @@ static uint8_t bDC_WriteTest(tDcState* pxState)
       pthread_join(pxState->xAllocatorThread, NULL);
       free(pxState->apMemBufs[0]);
       free(pxState->apMemBufs[1]);
+      sem_destroy(&(pxState->xSemThread));
+      sem_destroy(&(pxState->xSemBuffer0));
+      sem_destroy(&(pxState->xSemBuffer1));
       close(iFd);
 
       return 0;
     }
+    // Update counters and print info
     pxState->u64NowDataLeftBytes -= pxState->u32BufSize;
     DC_PrintProgress(pxState, 0);
   }
@@ -502,13 +471,16 @@ static uint8_t bDC_WriteTest(tDcState* pxState)
     if (u64WrittenCallBytes != u64LeftoverBytesToWrite)
     {
       printf("Error: Problem writing bytes %" PRIu64 "\n",
-	     pxState->u32BufSize * u64WriteBufferNum);
+	     pxState->u32BufSize * u64FullBuffersToWrite);
 
       pxState->u8WantBuffer = 100;
       sem_post(&(pxState->xSemThread));
       pthread_join(pxState->xAllocatorThread, NULL);
       free(pxState->apMemBufs[0]);
       free(pxState->apMemBufs[1]);
+      sem_destroy(&(pxState->xSemThread));
+      sem_destroy(&(pxState->xSemBuffer0));
+      sem_destroy(&(pxState->xSemBuffer1));
       close(iFd);
 
       return 0;
@@ -525,15 +497,220 @@ static uint8_t bDC_WriteTest(tDcState* pxState)
   printf("\nDone all writing!\n");
   close(iFd);
 
-  // Bogus value so thread exists:
+  // Bogus value so thread exits:
   pxState->u8WantBuffer = 100;
   sem_post(&(pxState->xSemThread));
   pthread_join(pxState->xAllocatorThread, NULL);
   free(pxState->apMemBufs[0]);
   free(pxState->apMemBufs[1]);
+  sem_destroy(&(pxState->xSemThread));
+  sem_destroy(&(pxState->xSemBuffer0));
+  sem_destroy(&(pxState->xSemBuffer1));
   
   return 1;
 }
+
+
+
+
+
+
+static uint8_t bDC_ReadTest(tDcState* pxState)
+{
+  uint64_t u64ReadCallBytes = 0;
+  uint64_t u64FullBuffersToRead = 0;
+  uint64_t u64LeftoverBytesToRead = 0;
+  uint64_t u64ReadBufferNum = 0;
+  int iFd = -1;
+
+  if ((sem_init(&(pxState->xSemThread), 0, 0) != 0) ||
+      (sem_init(&(pxState->xSemBuffer0), 0, 0) != 0) ||
+      (sem_init(&(pxState->xSemBuffer1), 0, 0) != 0))
+  {
+    printf("Failed to initialize semaphores\n");
+
+    return 1;
+  }
+
+  pthread_create(&(pxState->xAllocatorThread), NULL,
+		 (void*)bDC_BufferAllocator, pxState);
+
+  // Need to allocate both buffers
+  pxState->apMemBufs[0] = malloc(pxState->u32BufSize);
+  pxState->apMemBufs[1] = malloc(pxState->u32BufSize);
+
+  if ((pxState->apMemBufs[0] == NULL) ||
+      (pxState->apMemBufs[1] == NULL))
+  {
+    printf("Error: Malloc failed\n");
+    free(pxState->apMemBufs[0]);
+    free(pxState->apMemBufs[1]);
+    sem_destroy(&(pxState->xSemThread));
+    sem_destroy(&(pxState->xSemBuffer0));
+    sem_destroy(&(pxState->xSemBuffer1));
+    
+    return 0;
+  }
+  // Now it is time to set counters
+  pxState->u64CurrNumber = 0;
+  pxState->u64LastDataLeftBytes = pxState->u64DevSizeBytes;
+  pxState->u64NowDataLeftBytes = pxState->u64DevSizeBytes;
+
+  // Thread is now waiting instructions
+  pxState->u8WantBuffer = 0;
+  // We wake another thread and sleep ourselves
+  sem_post(&(pxState->xSemThread));
+  sem_wait(&(pxState->xSemBuffer0));
+  // Ok, allocator thread has allocated buf 0.
+  // In order for the reader loop
+  // to work as expected, buffer sem needs additional post.
+  sem_post(&(pxState->xSemBuffer0));
+
+  // Figure out how many full buffers and leftover bytes to read
+  u64FullBuffersToRead = pxState->u64DevSizeBytes / pxState->u32BufSize;
+  u64LeftoverBytesToRead = pxState->u64DevSizeBytes - (u64FullBuffersToRead * pxState->u32BufSize);
+
+  // FIXME: Correct additional flags
+  //iFd = open(pxState->sDevice, O_WRONLY | O_SYNC);
+  iFd = open(pxState->sDevice, O_RDONLY);
+
+  if (iFd == -1)
+  {
+    printf("Error: Unable to open the device in read mode\n");
+
+    return 0;
+  }
+  printf("Read test starting\n");
+  // Write couple of newlines in sync to the prevline sequences
+  printf("\n\n");
+
+  // Make initial zero print a bit earlier:
+  gettimeofday(&(pxState->xLastTime), NULL);
+  pxState->xLastTime.tv_sec -= (ADT_DC_PROGRESS_UPDATE_INTERVAL + 1);
+  // Actual start of routine loop
+  gettimeofday(&(pxState->xStartTime), NULL);
+  
+  // In loop prepare the buffer and read..
+  // ..but use buffers so that buffer0 is the compare buffer
+  // and buffer1 is the read buffer. So in other words:
+  // buffer0 = allocate
+  // buffer1 = read
+  for (u64ReadBufferNum = 0; u64ReadBufferNum < u64FullBuffersToRead; u64ReadBufferNum++)
+  {
+    u64ReadCallBytes = read(iFd, pxState->apMemBufs[1], pxState->u32BufSize);
+
+    if (u64ReadCallBytes != pxState->u32BufSize)
+    {
+      printf("Error: Problem reading bytes %" PRIu64 "\n",
+	     pxState->u32BufSize * u64ReadBufferNum);
+      
+      pxState->u8WantBuffer = 100;
+      sem_post(&(pxState->xSemThread));
+      pthread_join(pxState->xAllocatorThread, NULL);
+      free(pxState->apMemBufs[0]);
+      free(pxState->apMemBufs[1]);
+      sem_destroy(&(pxState->xSemThread));
+      sem_destroy(&(pxState->xSemBuffer0));
+      sem_destroy(&(pxState->xSemBuffer1));
+      close(iFd);
+      
+      return 0;
+    }
+    // Ensure after wait we have everything in compare buffer 0
+    sem_wait(&(pxState->xSemBuffer0));
+
+    // Aaand, compare it
+    if (memcmp(pxState->apMemBufs[0], pxState->apMemBufs[1], pxState->u32BufSize) != 0)
+    {
+      // TODO: Find out which byte exactly.
+      printf("\nError: Comparing failed at block beginning at %" PRIu64 "\n",
+	     pxState->u64DevSizeBytes - pxState->u64NowDataLeftBytes);
+
+      pxState->u8WantBuffer = 100;
+      sem_post(&(pxState->xSemThread));
+      pthread_join(pxState->xAllocatorThread, NULL);
+      free(pxState->apMemBufs[0]);
+      free(pxState->apMemBufs[1]);
+      sem_destroy(&(pxState->xSemThread));
+      sem_destroy(&(pxState->xSemBuffer0));
+      sem_destroy(&(pxState->xSemBuffer1));
+      close(iFd);
+      
+      return 0;
+    }
+    // Safely compared, allow another allocation
+    sem_post(&(pxState->xSemThread));
+    // Update counters and print info
+    pxState->u64NowDataLeftBytes -= pxState->u32BufSize;
+    DC_PrintProgress(pxState, 0);
+  }
+  if (u64LeftoverBytesToRead)
+  {
+    u64ReadCallBytes = read(iFd, pxState->apMemBufs[1],
+			    u64LeftoverBytesToRead);
+
+    if (u64ReadCallBytes != u64LeftoverBytesToRead)
+    {
+      printf("Error: Problem reading bytes %" PRIu64 "\n",
+	     pxState->u32BufSize * u64FullBuffersToRead);
+      
+      pxState->u8WantBuffer = 100;
+      sem_post(&(pxState->xSemThread));
+      pthread_join(pxState->xAllocatorThread, NULL);
+      free(pxState->apMemBufs[0]);
+      free(pxState->apMemBufs[1]);
+      sem_destroy(&(pxState->xSemThread));
+      sem_destroy(&(pxState->xSemBuffer0));
+      sem_destroy(&(pxState->xSemBuffer1));
+      close(iFd);
+      
+      return 0;
+    }
+    // Final compare
+    if (memcmp(pxState->apMemBufs[0], pxState->apMemBufs[1], u64LeftoverBytesToRead) != 0)
+    {
+      // TODO: Find out which byte exactly.
+      printf("\nError: Comparing failed at block beginning at %" PRIu64 "\n",
+	     pxState->u32BufSize * u64FullBuffersToRead);
+
+      pxState->u8WantBuffer = 100;
+      sem_post(&(pxState->xSemThread));
+      pthread_join(pxState->xAllocatorThread, NULL);
+      free(pxState->apMemBufs[0]);
+      free(pxState->apMemBufs[1]);
+      sem_destroy(&(pxState->xSemThread));
+      sem_destroy(&(pxState->xSemBuffer0));
+      sem_destroy(&(pxState->xSemBuffer1));
+      close(iFd);
+      
+      return 0;
+    }
+    pxState->u64NowDataLeftBytes -= u64LeftoverBytesToRead;
+  }
+  printf("\nSyncinc...\n\n\n");
+  fsync(iFd);
+  DC_PrintProgress(pxState, 1);
+  printf("\nDone all reading!\n");
+  close(iFd);
+
+  // Bogus value so thread exits:
+  pxState->u8WantBuffer = 100;
+  sem_post(&(pxState->xSemThread));
+  pthread_join(pxState->xAllocatorThread, NULL);
+  free(pxState->apMemBufs[0]);
+  free(pxState->apMemBufs[1]);
+  sem_destroy(&(pxState->xSemThread));
+  sem_destroy(&(pxState->xSemBuffer0));
+  sem_destroy(&(pxState->xSemBuffer1));
+  
+  return 1;
+}
+
+
+
+
+
+
 
 
 
@@ -554,15 +731,6 @@ int main(int argc, char* argv[])
   if (pxState == NULL)
   {
     printf("Failed to malloc state struct\n");
-
-    return 1;
-  }
-  if ((sem_init(&(pxState->xSemThread), 0, 0) != 0) ||
-      (sem_init(&(pxState->xSemBuffer0), 0, 0) != 0) ||
-      (sem_init(&(pxState->xSemBuffer1), 0, 0) != 0))
-  {
-    printf("Failed to initialize semaphores\n");
-    free(pxState);
 
     return 1;
   }
